62134cd2659f10a062bbd148b1da6c29757df053
Magp133 <ejrichardsward@gmail.com>
2024-09-04 09:43:37 +1200

    Filtered the magic items into Items_magic.csv using test.

--- tests.py
+++ tests.py
@@ -7,17 +7,14 @@ import pandas as pd
 # import the items database
 items = pd.read_csv('data/Items_magic.csv')
 
-# remove the items with no Value which is NA
-#extract the mundance items which do not have a rarity.
-# their rarity is 
-mundane_items = items[items['Rarity'] == 'none']
+# remove items that have a rarity of none.
+# creating the magical items database
+items = items[items['Rarity'] != 'none']
 
-#drop the rarity column
-mundane_items = mundane_items.drop(columns=['Rarity'])
-# drop the attunement table
-mundane_items = mundane_items.drop(columns=['Attunement'])
+# remove the value column
+items = items.drop(columns=['Value'])
 
-# save the items.csv file
-mundane_items.to_csv('data/Items.csv', index=False)
+# save the file
+items.to_csv('data/Items_magic.csv', index=False)
 
-print(mundane_items.head())
+print(items.head())
