a6fd1836e92251260e8169005a39b7e8291829fe
Magp133 <ejrichardsward@gmail.com>
2024-09-04 09:55:54 +1200

    items.csv now includes items with rarity none or unknown. both are mundane items

--- tests.py
+++ tests.py
@@ -6,15 +6,3 @@ import pandas as pd
 
 # import the items database
 items = pd.read_csv('data/Items_magic.csv')
-
-# remove items that have a rarity of none.
-# creating the magical items database
-items = items[items['Rarity'] != 'none']
-
-# remove the value column
-items = items.drop(columns=['Value'])
-
-# save the file
-items.to_csv('data/Items_magic.csv', index=False)
-
-print(items.head())
