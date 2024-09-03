86495d550c4120d66ac9a46996bd50feee86fab0
Magp133 <ejrichardsward@gmail.com>
2024-09-04 09:57:19 +1200

    removed unknown rarity items from items_magic

--- tests.py
+++ tests.py
@@ -6,3 +6,11 @@ import pandas as pd
 
 # import the items database
 items = pd.read_csv('data/Items_magic.csv')
+
+# remove rarities that are unknown
+items = items[items['Rarity'] != 'unknown']
+
+print(items.head())
+
+# save the items database
+items.to_csv('data/Items_magic.csv', index=False)
