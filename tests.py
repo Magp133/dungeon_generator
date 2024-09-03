c345b9356360ffaadfa49d4ece305f6f09d88358
Magp133 <ejrichardsward@gmail.com>
2024-09-04 09:41:10 +1200

    used tests.py to filter the items. both databases included all the items instead of already being sorted between magical and mundane. Items.csv is now mundane items.

--- tests.py
+++ tests.py
@@ -2,3 +2,22 @@
 File used to test and check the functionality of the code.
 """
 
+import pandas as pd
+
+# import the items database
+items = pd.read_csv('data/Items_magic.csv')
+
+# remove the items with no Value which is NA
+#extract the mundance items which do not have a rarity.
+# their rarity is 
+mundane_items = items[items['Rarity'] == 'none']
+
+#drop the rarity column
+mundane_items = mundane_items.drop(columns=['Rarity'])
+# drop the attunement table
+mundane_items = mundane_items.drop(columns=['Attunement'])
+
+# save the items.csv file
+mundane_items.to_csv('data/Items.csv', index=False)
+
+print(mundane_items.head())
